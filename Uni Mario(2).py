078aa3814f7080311060b86ae656af70db099772
smguo05 <56418495+smguo05@users.noreply.github.com>
2019-10-16 11:05:00 -0500

    Add files via upload

--- Uni Mario(2).py
+++ Uni Mario(2).py
@@ -1,21 +1,38 @@
 import pygame
+import sys
+import pygame.sprite as sprite
 pygame.init()
 
-win = pygame.display.set_mode((590, 369))
 pygame.display.set_caption("Uni Mario")
 
 
-x = 100
-y = 185
+m_x = 100
+m_y = 350
 width = 40
 height = 60
 vel = 5
 
 left=False
 right=False
-walk_count = 0
+walk_cousnt = 0
 walkRight = []
 walkLeft = []
+
+theClock = pygame.time.Clock()
+
+background_image = pygame.image.load('Sprites/bg1.png')
+background_size = background_image.get_size()
+background_rect = background_image.get_rect()
+sw = 840
+sh = 464
+win = pygame.display.set_mode((sw, sh))
+w,h = background_size
+
+bg_x1 = 0
+bg_y1 = 0
+
+bg_x2 = w
+bg_y2 = 0
 
 def load_img(file_name): # loads the image, makes the pure white background transparent
     img = pygame.image.load(file_name).convert()
@@ -26,10 +43,8 @@ def load_img(file_name): # loads the image, makes the pure white background tran
 for i in range(1,7):
     walkLeft.append( load_img("Sprites/L" + str(i) + ".png" ) ) #loads in lists of images
     walkRight.append( load_img("Sprites/R" + str(i) + ".png") ) 
-#walkRight = [pygame.transform.flip(img,True,False) for img in walkLeft] #flips left images to be right
 
 player_image = walkRight[0]
-background_image = pygame.image.load("Sprites/bg.jpg").convert()
 
 pygame.mixer.music.load('Music/bensound-summer.mp3')
 pygame.mixer.music.play(-1)
@@ -43,22 +58,32 @@ right_idx=0
 run = True #main loop
 while run:
     pygame.time.delay(50)
+    
+    win.blit(background_image,(sw, sh)) #makes a scrolling background 
+    pygame.display.update()
+    
+    bg_x2 -= 5
+    bg_x1 -= 5
+    if bg_x1 < sw - 2*w:
+       bg_x1 = 0
+    if bg_x2 < sw - w:
+       bg_x2 = w
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+                                             
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > vel: 
-        x -= vel
+    if keys[pygame.K_LEFT] and m_x > vel: 
+        m_x -= vel
         if not isJump:
             player_image = walkLeft[left_idx]
             left_idx += 1
             if left_idx >= len(walkLeft):
                 left_idx=0
         
-    if keys[pygame.K_RIGHT] and x < 590 - width - vel:
-        x += vel
+    if keys[pygame.K_RIGHT] and m_x < sw - width - vel:
+        m_x += vel
         if not isJump:
             player_image = walkRight[right_idx]
             right_idx += 1
@@ -76,17 +101,20 @@ while run:
             neg = 1
             if jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2)* 0.25 * neg
+            m_y -= (jumpCount ** 2)* 0.25 * neg
             jumpCount -= 1
 
         else:
             isJump = False
             jumpCount = 10
 
-    win.blit(background_image, [0, 0])
-    win.blit(player_image, (x,y))
+            
+    win.blit(background_image,(bg_x1,bg_y1))
+    win.blit(background_image,(bg_x2,bg_y2))
+    win.blit(player_image, (m_x,m_y))
   
     pygame.display.update()
+    theClock.tick(500)
 
 
         
