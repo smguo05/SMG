8ab8db57242ed13238da5905095619c76650042d
smguo05 <56418495+smguo05@users.noreply.github.com>
2019-10-23 11:10:03 -0500

    Add files via upload

--- Uni Mario(2).py
+++ Uni Mario(2).py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 import pygame.sprite as sprite
+import time
 pygame.init()
 
 pygame.display.set_caption("Uni Mario")
@@ -18,6 +19,11 @@ walk_count = 0
 walkRight = []
 walkLeft = []
 
+
+myfont = pygame.font.SysFont("monospace", 25)
+screen_over = pygame.font.SysFont("monospace", 100)
+score = 0
+
 theClock = pygame.time.Clock()
 
 background_image = pygame.image.load('Sprites/bg2.png')
@@ -27,6 +33,7 @@ sw = 840
 sh = 464
 win = pygame.display.set_mode((sw, sh))
 w,h = background_size
+
 
 t_x = sw
 t_y = 350
@@ -50,7 +57,7 @@ for i in range(1,7):
     walkRight.append( load_img("Sprites/R" + str(i) + ".png") ) 
 
 player_image = walkRight[0]
-turtle_image = pygame.image.load('Sprites/turtle.png')
+turtle_image = pygame.image.load('Sprites/EL2.png')
 turtle_small = pygame.transform.scale(turtle_image, (100, 60))
 
 pygame.mixer.music.load('Music/bensound-summer.mp3')
@@ -76,7 +83,7 @@ while run:
        if bg_x2 < sw - 2*w:
           bg_x2 = sw
 
-    t_x -=10
+    t_x -=20
     if t_x < 0:
         t_x = sw
 
@@ -114,7 +121,7 @@ while run:
             neg = 1
             if jumpCount < 0:
                 neg = -1
-            m_y -= (jumpCount ** 2)* 0.25 * neg
+            m_y -= (jumpCount ** 2)* 0.4 * neg
             jumpCount -= 1
 
         else:
@@ -127,14 +134,24 @@ while run:
         win.blit(background_image,(bg_x2,bg_y2))
     win.blit(player_image, (m_x,m_y))
     win.blit(turtle_small, (t_x, t_y))
+
+    label = myfont.render("Score = "+ str(score), 1, (0, 0, 0)) #NEED TO FIX
+    win.blit(label, ((sw-200), 50))
+
   
     pygame.display.update()
 
    
     d2 = (t_x - m_x)**2 + (t_y - m_y)**2
-    if d2 < 1000:
-        run = False
-        
+    if d2 < 100:
+         score -=1
+         if score < -5:
+             label2 = screen_over.render("Game Over", 1, (255, 0, 0))
+             win.blit(label2, (150, 200))
+             pygame.display.update()
+             time.sleep(3)
+             run= False
+    
 
     theClock.tick(50)
 
