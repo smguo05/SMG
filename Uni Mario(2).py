6cf35ebf22b06dd45b1ca1acbfaa17a10f78b360
smguo05 <56418495+smguo05@users.noreply.github.com>
2019-11-06 10:51:54 -0600

    Add files via upload

--- Uni Mario(2).py
+++ Uni Mario(2).py
@@ -18,8 +18,6 @@ right=False
 walk_count = 0
 walkRight = []
 walkLeft = []
-walkELeft = []
-
 
 myfont = pygame.font.SysFont("monospace", 25)
 screen_over = pygame.font.SysFont("monospace", 100)
@@ -38,6 +36,7 @@ w,h = background_size
 
 g_x = sw
 g_y = 360
+g_v = 15
 
 bg_x1 = 0
 bg_y1 = 0
@@ -46,6 +45,7 @@ bg_x2 = w
 bg_y2 = 0
 
 d2 = 9999
+count = 9999
 
 def load_img(file_name): # loads the image, makes the pure white background transparent
     img = pygame.image.load(file_name).convert()
@@ -58,16 +58,16 @@ for i in range(1,7):
     walkRight.append( load_img("Sprites/R" + str(i) + ".png") ) 
 player_image = walkRight[0]
 
+slide = [pygame.image.load('Sprites/S1.png'), pygame.image.load('Sprites/S1.png')] #CHANGE WITH S1 WHEN AVAILABLE!!
 enemy_images = [pygame.image.load('Sprites/G1.png'), pygame.image.load('Sprites/G2.png')]
+
 index = 0
-                                   
-pygame.mixer.music.load('Music/bensound-summer.mp3')
+
+pygame.mixer.music.load('Music/bensound-summer.mp3') #runs music on loop
 pygame.mixer.music.play(-1)
 
 isJump = False
 jumpCount = 10
-isSlide = False
-slideCount = 10
 
 left_idx=0
 right_idx=0
@@ -83,36 +83,30 @@ while run:
     if bg_x2 < sw - 2*w:
        bg_x2 = sw
 
-    g_x -=20 #respawns Goomba every background frame
+    g_x -= g_v #respawns Goomba every background frame
     if g_x < 0:
         g_x = sw
-
-   
-          
+        if g_v < 100:
+            g_v += 3
+            
+         
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False
-            
-    player_image = walkRight[right_idx] #running by himself
-    right_idx += 1
-    if right_idx >= len(walkRight):
-        right_idx=0                                         
+            run = False                                       
 
     keys = pygame.key.get_pressed()
-        
-    #if keys[pygame.K_RIGHT] and p_x < sw - width - vel:
-     #  p_x += vel
-      # if not isJump:
-       #     player_image = walkRight[right_idx]
-        #    right_idx += 1
-         #   if right_idx >= len(walkRight):
-          #      right_idx=0
+    if run == True:
+        if not isJump:
+            player_image = walkRight[right_idx]
+            right_idx += 1
+            if right_idx >= len(walkRight):
+                right_idx=0
                 
     if not(isJump): #jumping animation
         if keys[pygame.K_UP]:
             isJump = True
-        #if keys[pygame.K_SPACE]: #sliding animation [IN PROGRESS]
-            #isJump = True
+        if keys[pygame.K_SPACE]:
+            isJump = True
 
     else:
         if jumpCount >= -10:
@@ -126,14 +120,20 @@ while run:
             isJump = False
             jumpCount = 10
 
-    if keys[pygame.K_DOWN]:
-            isSlide = True
+    if not(isJump): #sliding animation
+        if keys[pygame.K_DOWN]:
+            player_image = slide[index]
+            index +=1
+            if index >= len(slide):
+                index = 0
+            
 
     if bg_x1 > -w:        
         win.blit(background_image,(bg_x1,bg_y1))
     if bg_x2 > -w:
         win.blit(background_image,(bg_x2,bg_y2))
     win.blit(player_image, (p_x,p_y))
+    
     if run==True: #animation of Goomba
         win.blit(enemy_images[index], (g_x, g_y))
         pygame.time.delay(20)
@@ -147,10 +147,11 @@ while run:
   
     pygame.display.update()
 
-   
+    if count < 9999: count += 1
     d2 = (g_x - p_x)**2 + (g_y - p_y)**2 #calculates distance between the Mario and Goomba
-    if d2 < 500:
+    if d2 < 5000 and count > 10: #adds only one point per hit
          hit_count +=1
+         count = 0
          if hit_count > 5:
              label2 = screen_over.render("Game Over", 1, (255, 0, 0))
              win.blit(label2, (150, 200))
